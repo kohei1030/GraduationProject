a91f043ca4fac6f66df367dc96a8b2f065b6631e
kohei1030 <climate_1030_kk@yahoo.co.jp>
2016-10-15 13:20:47 +0900

    add script

---
+++ UnityProject/GraduationProject/WorkSpace/俺/Status.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Status : MonoBehaviour {
+	[SerializeField]
+	private float _moveSpeed = 1;
+	public float GetMoveSpeed(){
+		retun _moveSpeed;
+	}
+	[SerializeField]
+	private int _life = 1;
+	public int GetLife(){
+		return _life;
+	}
+}
+
