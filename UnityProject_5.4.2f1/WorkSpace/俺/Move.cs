9b45335cea9c0b6112a8daca0295c03b10a19980
kohei1030 <climate_1030_kk@yahoo.co.jp>
2016-10-16 05:50:24 +0900

    UnityVersion5.4.2f1

---
+++ UnityProject_5.4.2f1/WorkSpace/俺/Move.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	private Status _enemyStatus;
+
+	void Start () {
+		_enemyStatus = GetComponent<Status> ();
+		float moveSpeed = _enemyStatus.GetMoveSpeed ();
+	}
+}
