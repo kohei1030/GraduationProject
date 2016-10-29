7c4a59190589706ee03aaf90d56f15ceb97d0f83
jubirosaiko <jubirosaiko@yahoo.co.jp>
2016-10-29 11:37:12 +0900

    add player

---
+++ UnityProject_5.4.2f1/WorkSpace/むらびと/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    
+    [SerializeField]
+    int _playerHp = 1;
+
+
+	void Start () {
+        _playerHp = 1;
+	}
+	
+	void Update () {
+        if (_playerHp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "Enemy") {
+            _playerHp -= 1;
+
+        }
+
+      
+    }
+
+}
---
+++ UnityProject_5.4.2f1/WorkSpace/むらびと/Scripts/Shooting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject bullet;
+
+    //public Transform muzzle;
+
+    
+    // Use this for initialization
+    void Start () {
+       // Shoot();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Shoot();
+    }
+
+    void Shoot() {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+          GameObject  bullets = GameObject.Instantiate(bullet) as GameObject;
+            //bullets.transform.position = muzzle.position;
+            
+        }
+
+    }
+    
+}
