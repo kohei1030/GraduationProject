66a132a5f3a193d514ff335fd809db5beba4652f
kohei1030 <climate_1030_kk@yahoo.co.jp>
2017-02-12 21:50:45 +0900

    metaファイル等がはじかれてるため、prefab等セットしなおさないとかも

---
+++ UnityProject_5.4.2f1/Assets/WorkSpace/ore/backGround/CreateStar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CreateStar : MonoBehaviour {
+
+    [SerializeField]
+    private List<GameObject> _stars;
+
+    private List<GameObject> _dispStar;
+
+    [SerializeField]
+    private float _createSpeed;
+
+    private float _count = 0;
+
+	void Update () {
+        _count -= Time.deltaTime;
+        if (_count <= 0)
+        {
+            GameObject star = _stars[(int)UnityEngine.Random.Range(0, _stars.Count)];
+
+            Vector2 minPos = new Vector2(transform.position.x - transform.localScale.x / 2, transform.position.y - transform.localScale.y / 2);
+            Vector2 maxPos = new Vector2(transform.position.x + transform.localScale.x / 2, transform.position.y + transform.localScale.y / 2);
+
+            Vector3 pos = new Vector3(UnityEngine.Random.Range(minPos.x, maxPos.x), UnityEngine.Random.Range(minPos.y, maxPos.y), transform.position.z);
+
+            GameObject.Instantiate(star).transform.position = pos;
+
+            _count = _createSpeed;
+        }
+	}
+}
---
+++ UnityProject_5.4.2f1/Assets/WorkSpace/ore/backGround/StarManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarManager : MonoBehaviour {
+
+    [SerializeField]
+    private float _maxVal;
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _dispTime;
+
+    private Vector3 addVal;
+
+	void Start () {
+        transform.localScale = new Vector3(0.0f, 0.0f, 1.0f);
+        addVal = new Vector3(_speed, _speed, 0.0f);
+	}
+	
+	void Update () {
+        transform.localScale += addVal;
+
+        if (transform.localScale.x > _maxVal || transform.localScale.x < 0)
+        {
+            addVal *= -1;
+        }
+
+        _dispTime -= Time.deltaTime;
+        if (_dispTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+	}
+}
